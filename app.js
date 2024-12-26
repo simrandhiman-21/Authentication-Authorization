d5e44723a58b0ed29964926e36767eeec0413ef7
simrandhiman <simrandhiman3121@gmail.com>
2024-12-26 13:09:51 +0530

    bcrypt_passwordEncrypt

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-//set cookiie and reading 
+// //set cookiie and reading 
+
+// const express = require('express');
+// const app = express();
+// const cookieParser = require('cookie-parser'); // Corrected import
+
+// app.use(cookieParser()); // Middleware to parse cookies
+
+// // Route to set a cookie
+// app.get('/', function (req, res) {
+//     res.cookie("name", "simrandhiman"); // Set a cookie with key "name" and value "simrandhiman"
+//     res.send("Cookie has been set");
+// });
+
+// //read -req
+// //set= res
+
+
+// // Route to read cookies
+// app.get('/read', function (req, res) {
+//     console.log(req.cookies); // Logs all cookies sent by the client
+//     res.send("Cookies are logged in the console");
+// });
+
+
+// app.listen(3000);
+
+
+//bcrypt use karte hai password to encrypt
 const express = require('express');
 const app = express();
-const cookieParser = require('cookie-parser'); // Corrected import
+const bcrypt=require('bcrypt');
 
-app.use(cookieParser()); // Middleware to parse cookies
-
-// Route to set a cookie
 app.get('/', function (req, res) {
-    res.cookie("name", "simrandhiman"); // Set a cookie with key "name" and value "simrandhiman"
-    res.send("Cookie has been set");
+    bcrypt.genSalt(10, function(err, salt) {
+        // console.log(salt) //random string
+        bcrypt.hash("polololololo", salt, function(err, hash) {
+            // Store hash in your password DB.
+            console.log(hash);
+        });
+    });
+    
 });
 
-//read -req
-//set= res
-
-
-// Route to read cookies
-app.get('/read', function (req, res) {
-    console.log(req.cookies); // Logs all cookies sent by the client
-    res.send("Cookies are logged in the console");
-});
-
-// Start the server
 app.listen(3000);
