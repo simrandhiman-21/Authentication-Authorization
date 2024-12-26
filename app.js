8116b7ea804a9afe1b139233d636fe8c94703713
simrandhiman <simrandhiman3121@gmail.com>
2024-12-26 13:45:15 +0530

    JWT

--- app.js
+++ app.js
@@ -49,19 +49,44 @@
 
 
 
-//decrypt
+// decrypt
+// const express = require('express');
+// const app = express();
+// const bcrypt=require('bcrypt');
+
+// app.get('/', function (req, res) {
+//     bcrypt.compare("polololololo", "$2b$10$Q5fdDR2Sqg2NVS4WQMP9vuel/2fYb1ef.2RSRAsEC4TgPfCjJuO2.", function(err, result) {
+//         // result == true
+//         console.log(result);
+//     });
+    
+//     res.send("I'm home page")
+    
+// });
+
+// app.listen(3000);
+
+
+//JWT 
 const express = require('express');
 const app = express();
-const bcrypt=require('bcrypt');
+const cookieParser=require('cookie-parser');
+const jwt=require('jsonwebtoken');
+
+app.use(cookieParser());
 
 app.get('/', function (req, res) {
-    bcrypt.compare("polololololo", "$2b$10$Q5fdDR2Sqg2NVS4WQMP9vuel/2fYb1ef.2RSRAsEC4TgPfCjJuO2.", function(err, result) {
-        // result == true
-        console.log(result);
-    });
-    
-    res.send("I'm home page")
-    
+    let token=jwt.sign({email:"simran@example.com"},"secret");
+    res.cookie("token",token);
+    // console.log(token);
+    res.send("check token string");  
 });
+
+app.get("/read",function(req,res){
+    // console.log(req.cookies.token);
+    let data=jwt.verify(req.cookies.token,"secret");
+    console.log(data);
+    res.send("I'm read ")
+})
 
 app.listen(3000);
